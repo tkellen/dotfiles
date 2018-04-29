9b4bdc2a29658894e19c7eb95943ebce9d5c6cee
Tyler Kellen <tyler@sleekcode.net>
2018-04-29 17:56:01 -0400

    process completions through k alias

--- shell/zsh/k8s.zsh
+++ shell/zsh/k8s.zsh
@@ -6,3 +6,6 @@ alias kl="k logs"
 alias ke="k exec"
 alias ks="k --namespace=kube-system"
 alias kbb="k run -i --tty busybox --image=busybox --restart=Never --rm -- sh"
+
+# ensure k alias completes using kubectl completions
+compdef k='kubectl'
