ba245c0d3c8b25b86712b846c43f9056ff57a4a5
Tyler Kellen <tyler@sleekcode.net>
2019-05-02 17:37:54 -0400

    misc

--- shell/zsh/k8s.zsh
+++ shell/zsh/k8s.zsh
@@ -1,11 +1,7 @@
 alias k="kubectl"
-alias ka="k apply -f"
-alias kg="k get"
-alias kd="k describe"
-alias kl="k logs"
-alias ke="k exec"
-alias ks="k --namespace=kube-system"
+alias ks="kubectl --namespace=kube-system"
 alias kbb="k run -i --tty busybox --image=busybox --restart=Never --rm -- sh"
 
 # ensure k alias completes using kubectl completions
-compdef k='kubectl'
+compdef k="kubectl"
+compdef ks="kubectl"
